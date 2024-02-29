49477ba5f57ea0dda36d5e39a15fccf6fb43bb7f
samirm00 <sammoham.be@gmail.com>
2024-02-29 12:44:21 +0100

    initial commit

---
+++ course/hello_world.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+# What is shell?
+# Shell is a program that takes commands from the keyboard and gives them to the operating system to perform.
+
+# What is a shell script?
+# A shell script is a computer program designed to be run by the Unix shell, a command-line interpreter.
+
+# Print Hello, World!
+echo "Hello, World!"
+
+# Print the home directory
+echo $HOME
+
+# Print the current working directory
+echo $PWD
+
+
+
+
