dabf0ab98fcbaa097bd32da365fc2f3737fc4ac3
samirm00 <sammoham.be@gmail.com>
2024-03-05 05:43:46 +0100

    add how bash process commands

--- 03-how-bash-process-commands/01.sh
+++ 03-how-bash-process-commands/01.sh
@@ -3,5 +3,12 @@
 # Author: Samir M.
 # Date: 2024-03-04
 # Modified: 2024-03-04
-# Description: Brace expansion
+# Description: How bash processes command lines
 # Usage: ./01.sh
+
+
+# Bash goes through 5 steps to interpret a command line.
+
+# Step 1: Tokenisation:
+# The command line is broken into tokens. Tokens are the smallest units of a command line.
+# Tokens are separated by metacharacters, such as spaces, tabs, and newlines, | ; & ( ) < >.
